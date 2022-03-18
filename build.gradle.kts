09ad21f6aeccffc62960bee05cd07f02c4ffe4b8
Vasya <t.lila@list.ru>
2022-03-18 04:06:05 +0300

    👷‍♂️ Add publishing to maven local

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,29 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("maven-publish")
+    id("signing")
+    id("org.jetbrains.dokka") version "1.5.30"
+}
+
+
+val dokkaOutputDir = "$buildDir/dokka"
+
+tasks.dokkaHtml {
+    outputDirectory.set(file(dokkaOutputDir))
+}
+
+val deleteDokkaOutputDir by tasks.register<Delete>("deleteDokkaOutputDirectory") {
+    delete(dokkaOutputDir)
+}
+
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    dependsOn(deleteDokkaOutputDir, tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaOutputDir)
 }
 
 allprojects {
-    plugins.apply("org.jetbrains.kotlin.jvm")
-
     group = "io.github.vasya-polyansky"
     version = "0.1.0"
 
@@ -16,8 +34,53 @@ allprojects {
 
     tasks.withType<KotlinCompile> {
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "1.8"
             freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+        }
+    }
+}
+
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("maven-publish")
+        plugin("signing")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                artifact(javadocJar)
+                pom {
+                    name.set("bot-framework")
+                    url.set("https://github.com/vasya-polyansky/bot-framework")
+                    developers {
+                        developer {
+                            id.set("vasya-polyansky")
+                            name.set("Vasya Polyansky")
+                            email.set("vasya1polyansky@gmail.com")
+                        }
+                    }
+
+                    scm {
+                        connection.set("scm:git:git@github.com:vasya-polyansky/bot-framework.git")
+                        developerConnection.set("scm:git:git@github.com:vasya-polyansky/bot-framework.git")
+                        url.set("https://github.com/vasya-polyansky/bot-framework.git")
+                    }
+                }
+            }
+        }
+
+        repositories {
+            mavenLocal()
+            maven {
+                name = "OSSRH"
+                setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                credentials {
+                    username = System.getenv("SONATYPE_USERNAME")
+                    password = System.getenv("SONATYPE_PASSWORD")
+                }
+            }
         }
     }
 }
